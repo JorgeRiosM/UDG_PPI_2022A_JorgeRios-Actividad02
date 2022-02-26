decd21a5931b85460ef2dfc2fb17f2d234b75acf
Jorge Rios <jorge.rios5539@alumnos.udg.mxgit config --global user.email>
2022-02-26 16:53:58 -0600

    final

---
+++ main.js
@@ -0,0 +1,41 @@
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const renderer = new THREE.WebGLRenderer({ antialias: true})
+
+renderer.setSize( window.innerWidth, window.innerHeight )
+renderer.setClearColor("#4d194d")
+document.body.appendChild( renderer.domElement )
+camera.position.z = 7
+window.addEventListener( 'resize', () => {
+	let width = window.innerWidth
+	let height = window.innerHeight
+	renderer.setSize( width, height )
+	camera.aspect = width / height
+	camera.updateProjectionMatrix()
+})
+var geometry = new THREE.BoxGeometry( 1.2, 1.2, 1.2)
+var material = new THREE.MeshStandardMaterial( { color: 0x590d22, flatShading: true, metalness: 0, roughness: 1 })
+var cube = new THREE.Mesh ( geometry, material )
+scene.add( cube )
+var geometry = new THREE.BoxGeometry( 3.5, 3.5, 3.5)
+var material = new THREE.MeshBasicMaterial( {
+	color: "#0x590d22", wireframe: true, transparent: true
+})
+var wireframeCube = new THREE.Mesh ( geometry, material )
+scene.add( wireframeCube )
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.2)
+scene.add( ambientLight )
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
+pointLight.position.set( 25, 50, 25 );
+scene.add( pointLight );
+
+
+function animate() {
+	requestAnimationFrame( animate )
+	cube.rotation.x += 0.04;
+	cube.rotation.y += 0.04;
+	wireframeCube.rotation.x -= 0.01;
+	wireframeCube.rotation.y -= 0.01;
+	renderer.render( scene, camera )
+}
+animate()
